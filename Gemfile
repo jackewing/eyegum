1bb796d67e8e7796ab9dce08b076f05a65e5aff8
Jack Ewing <jackewing23@gmail.com>
2020-08-19 17:03:10 +1200

    use slim middleman template

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 4.2'
-gem 'middleman-autoprefixer', '~> 2.7'
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
+gem 'middleman'
+gem 'middleman-autoprefixer'
+gem 'middleman-blog'
+gem 'middleman-livereload'
+
+gem 'slim'
+
+gem 'tzinfo-data', platforms: [:mswin, :x64_mingw, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :x64_mingw, :mingw]
--- config.rb
+++ config.rb
@@ -1,10 +1,20 @@
+#Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
+require "uglifier"
+
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+# Use '#id' and '.classname' as div shortcuts in slim
+# http://slim-lang.com/
+Slim::Engine.set_options shortcut: {
+  '#' => { tag: 'div', attr: 'id' }, '.' => { tag: 'div', attr: 'class' }
+}
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :livereload
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -12,6 +22,8 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page "/partials/*", layout: false
+page "/admin/*", layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
@@ -19,28 +31,53 @@ page '/*.txt', layout: false
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+# proxy artist.yml files to artist.html
+data.artists.each do |_filename, artist|
+  # artist is an array: [filename, {data}]
+  proxy "/artist/#{artist[:title].parameterize}/index.html", "artist.html",
+  locals: {artist: artist},
+  layout: 'artist-detail',
+  ignore: true
+end
 
 # Helpers
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+# pretty urls
+activate :directory_indexes
+
+helpers do
+  #helper to set background images with asset hashes in a style attribute
+  def background_image(image)
+    "background-image: url('" << image_path(image) << "')"
+  end
+
+  def nav_link(link_text, url, options = {})
+    options[:class] ||= ""
+    options[:class] << " active" if url == current_page.url
+    link_to(link_text, url, options)
+  end
+
+  def markdown(content)
+     Tilt['markdown'].new { content }.render
+  end
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  # Minify css on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript, ignore: "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
+
+  # Use Gzip
+  activate :gzip
+
+  #Use asset hashes to use for caching
+  #activate :asset_hash
+
+end
